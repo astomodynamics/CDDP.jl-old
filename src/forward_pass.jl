a19a30a76d61f3f5545b03337e4b632892cf3f9b
Tomo <astomodynamics.icloud.com>
2023-05-22 22:18:20 -0700

    modified cddp solver

--- src/forward_pass.jl
+++ src/forward_pass.jl
@@ -112,83 +112,87 @@ function forward_pass_ddp!(
     nothing
 end
 
-# """
-#     forward_pass_cddp()
-# """
-# function forward_pass_cddp!(
-#     sol::DDPSolutions,
-#     prob::AbstractDDPProblem,
-#     params::CDDPParameter;
-# )   
-#     X, U, Λ, Y, J = sol.X, sol.U, sol.Λ, sol.Y, sol.J
-#     l, L = sol.gains.l, sol.gains.L
-#     m, M, n, N = sol.gains.m, sol.gains.M, sol.gains.n, sol.gains.N
-#     dt = prob.dt
+"""
+    forward_pass_cddp()
+"""
+function forward_pass_cddp!(
+    sol::DDPSolutions,
+    prob::AbstractDDPProblem,
+    params::CDDPParameter;
+)   
+    X, U, Λ, Y, J = sol.X, sol.U, sol.Λ, sol.Y, sol.J
+    l, L = sol.gains.l, sol.gains.L
+    m, M, n, N = sol.gains.m, sol.gains.M, sol.gains.n, sol.gains.N
+    tf, dt, tN = prob.tf, prob.dt, prob.tN
     
-#     for step in params.line_search_steps
-#         x_new = prob.x_init
-#         X_new::Vector{Vector{Float64}} = Vector[]
-#         U_new::Vector{Vector{Float64}} = Vector[]
-#         Λ_new::Vector{Vector{Float64}} = Vector[]
-#         Y_new::Vector{Vector{Float64}} = Vector[]
-#         push!(X_new, x_new)
-#         for k in 0:prob.tN-1
-#             t = k*dt
-#             # deviation from nominal trajectory
-#             δx = x_new - X(t) 
+    for step in params.line_search_steps
+        x_new = prob.x_init
+        X_new::Vector{Vector{Float64}} = Vector[]
+        U_new::Vector{Vector{Float64}} = Vector[]
+        Λ_new::Vector{Vector{Float64}} = Vector[]
+        Y_new::Vector{Vector{Float64}} = Vector[]
+        push!(X_new, x_new)
+
+        for k in 0:tN-1
+            t = k*dt
+            # deviation from nominal trajectory
+            δx = x_new - X(t) 
             
-#             # update local optimal control
-#             u_new = U(t) + step * l(t) + L(t) * δx
+            # update local optimal control
+            u_new = U(t) + step * l(t) + L(t) * δx
 
-#             # update Lagrange multiplier
-#             λ_new = Λ(t) + step * m(t) + M(t) * δx
+            # update Lagrange multiplier
+            λ_new = Λ(t) + step * m(t) + M(t) * δx
 
-#             # propagate the next state
-#             p = ODEParams(prob.model, u_new, isarray=true)
-#             x_new += rk4_step(prob.f!, x_new, p, t, h=dt) * dt
+            # propagate the next state
+            p = ODEParams(prob.model, u_new, isarray=true)
+            x_new += rk4_step(prob.f!, x_new, p, t, h=dt) * dt
 
-#             # save new trajectory
-#             push!(X_new, x_new)
-#             push!(U_new, u_new)
-#             push!(Λ_new, λ_new)
+            # save new trajectory
+            push!(X_new, x_new)
+            push!(U_new, u_new)
+            push!(Λ_new, λ_new)
 
-#             if !params.isfeasible
-#                 # update perturbation vector
-#                 y_new = Y(t) + step * n(t) + N(t) * δx
-#                 push!(Y_new, y_new)
-#             else
-#                 push!(Y_new, zeros(prob.λ_dim))
-#             end
+            if !params.isfeasible
+                # update perturbation vector
+                y_new = Y(t) + step * n(t) + N(t) * δx
+                push!(Y_new, y_new)
+            else
+                push!(Y_new, zeros(prob.λ_dim))
+            end
             
-#         end
+        end
+        push!(U_new, zeros(prob.u_dim)) 
+        push!(Λ_new, zeros(prob.λ_dim))
+        push!(Y_new, zeros(prob.λ_dim))
         
-#         # convert X and U array to continuous function
-#         X_new_func = linear_interpolation((collect(LinRange(0.0, prob.tf, prob.tN+1)),), X_new, extrapolation_bc = Line())
-#         U_new_func = linear_interpolation((collect(LinRange(0.0, prob.tf, prob.tN)),), U_new, extrapolation_bc = Line())
-#         Λ_new_func = linear_interpolation((collect(LinRange(0.0, prob.tf, prob.tN)),), Λ_new, extrapolation_bc = Line())
-#         Y_new_func = linear_interpolation((collect(LinRange(0.0, prob.tf, prob.tN)),), Y_new, extrapolation_bc = Line())
 
-#         # evaluate the new trajectory
+        # convert X and U array to continuous function
+        X_new_func =  ConstantInterpolation(X_new, 0:dt:tf)
+        U_new_func =  ConstantInterpolation(U_new, 0:dt:tf)
+        Λ_new_func =  ConstantInterpolation(Λ_new, 0:dt:tf)
+        Y_new_func =  ConstantInterpolation(Y_new, 0:dt:tf)
+
+        # evaluate the new trajectory
+        isfeasible_new = get_feasibility(prob, X_new_func, U_new_func)
         
-#         isfeasible_new = get_feasibility(prob, X_new_func, U_new_func)
-        
-#         if isfeasible_new
-#             J_new = get_trajectory_log_cost(prob, params, X_new_func, U_new_func, Y_new_func, params.isfeasible)
-#         else
-#             J_new = J
-#         end
+        if isfeasible_new
+            J_new = get_trajectory_log_cost(prob, params, X_new_func, U_new_func, Y_new_func, params.isfeasible)
+        else
+            J_new = J
+        end
 
-#         # J_new = get_trajectory_cost(X_new_func, U_new_func, nothing, prob.x_final, prob.ell, prob.ϕ, prob.tN, prob.dt) 
-#         if isfeasible_new && J_new < J
-#             sol.X = X_new_func
-#             sol.U = U_new_func
-#             sol.Λ = Λ_new_func
-#             sol.Y = Y_new_func
-#             sol.J = J_new
-#             return nothing
-#         end
-#     end
+        # J_new = get_trajectory_cost(X_new_func, U_new_func, nothing, prob.x_final, prob.ell, prob.ϕ, prob.tN, prob.dt) 
+        if isfeasible_new && J_new < J
+            sol.X = X_new_func
+            sol.U = U_new_func
+            sol.Λ = Λ_new_func
+            sol.Y = Y_new_func
+            sol.J = J_new
+            return nothing
+        end
+    end
 
-#     nothing
-# end
+    nothing
+end
 
